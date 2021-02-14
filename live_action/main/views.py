c6dd89d5db7b2a666ab9dc2fd69e4dab6b8fbc67
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-14 12:53:08 +0300

    Added mainapp templates

--- live_action/main/views.py
+++ live_action/main/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h3>HELLO WORLD</h3>")
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse("<h3>About</h3>")
+    return render(request, 'main/about.html')
