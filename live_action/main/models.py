5c8e848721afb78452ec5895b5d3fb5cb692c238
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-15 21:00:29 +0300

    Added activity model

--- live_action/main/models.py
+++ live_action/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Places(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    type = models.CharField(max_length=50)
+    user_skill = models.IntegerField()  # 0-beginner,.., 2-pro
+    enviroment_chars = models.IntegerField()  # 0-wild lands,..,2-city
+    extreme = models.IntegerField()
+
+    def __str__(self):
+        return self.type
