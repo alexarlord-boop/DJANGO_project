9235b2c4bbf0a4b6257bbff03aaf8cd6f2850cd2
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-15 21:09:20 +0300

    Fixed activity model

--- live_action/main/models.py
+++ live_action/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Activities(models.Model):
+class Activity(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     coords = models.TextField()
