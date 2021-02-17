5788a328f47b2a63db505ed3aaba12b7ccb68ed1
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-17 21:46:28 +0300

    Refactored map.html, views.py

--- live_action/main/views.py
+++ live_action/main/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .forms import FilterForm
 from .models import Activity
+import folium
 
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        form = FilterForm(request.POST)
+        form = FilterForm(request.POST)  # получаем данные из формы -> запрос к бд
         if form.is_valid():
             # действия с данными фильтра
             return HttpResponseRedirect('/map')
@@ -28,4 +29,10 @@ def goals(request):
 
 
 def map(request):
-    return render(request, 'main/map.html')
+    map = folium.Map(location=[37.296933, -121.9574983], zoom_start=8,
+                     height='70%', width='100%')
+    folium.Marker(location=[37.4074687, -122.086669], popup="Google HQ", icon=folium.Icon(color='gray')).add_to(map)
+
+    map = map._repr_html_()
+    context = {'map': map}
+    return render(request, 'main/map.html', context)
