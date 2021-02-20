a68d32bf00bd09e4edb70adc9d944178339ebba8
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-20 14:52:08 +0300

    Refactored map view

--- live_action/main/views.py
+++ live_action/main/views.py
@@ -39,11 +39,16 @@ def goals(request):
 
 
 def map(request):
-    map = folium.Map(location=[37.296933, -121.9574983],
-                     zoom_start=8,
+    base_map = folium.Map(location=[40, 40],
+                     zoom_start=2,
                      height='70%', width='100%')
+    activities = Activity.objects.all()
+    for el in activities:
+        location = [float(el.coords.split(',')[0]), float(el.coords.split(',')[1])]
+        folium.Marker(location=location, popup=el.title, icon=folium.Icon(color=colors[el.type])).add_to(base_map)
 
-    map = map._repr_html_()
+
+    map = base_map._repr_html_()
     context = {'map': map}
     return render(request, 'main/map.html', context)
 
@@ -51,12 +56,12 @@ def map(request):
 def make_map_with_filter_options(post_data):
     base_map = folium.Map(location=[50.296933, 40.9574983], zoom_start=2,
                           height='100%', width='100%')
-
+    # add_activities_to_db(all_activities)
     activities = Activity.objects.filter(type__exact=post_data['type'],
                                          user_skill__lte=post_data['user_skill'],
                                          enviroment_chars__exact=post_data['enviroment_chars'],
                                          extreme__lte=post_data['extreme'])
-    print(activities)
+    # print(activities)
     for el in activities:
         location = [float(el.coords.split(',')[0]), float(el.coords.split(',')[1])]
         folium.Marker(location=location, popup=el.title, icon=folium.Icon(color=colors[el.type])).add_to(base_map)
