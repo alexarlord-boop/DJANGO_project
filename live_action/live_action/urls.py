5e1d9f0798392c0a91ff7684aae2d0b7b6410de0
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-26 16:36:18 +0300

    Added registration urls

--- live_action/live_action/urls.py
+++ live_action/live_action/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
