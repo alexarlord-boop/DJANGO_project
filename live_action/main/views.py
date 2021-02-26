30f3946d40ad25d1a14cd26f601cdb5fe7a1c486
alexarlord-boop <rezerw0.1@yandex.ru>
2021-02-26 21:23:11 +0300

    Added points list

--- live_action/main/views.py
+++ live_action/main/views.py
@@ -15,15 +15,17 @@ def index(request):
                      height='100%',
                      width='100%')._repr_html_()  # base map
     form = FilterForm()
+    points = list()
     if request.method == 'POST':
         form = FilterForm(request.POST)  # получаем данные из формы -> запрос к бд
         if form.is_valid():
             # действия с данными фильтра
-            map = make_map_with_filter_options(request.POST)
+            map, points = make_map_with_filter_options(request.POST)
 
     context = {
         'form': form,
         'map': map,
+        'points': points,
     }
 
     return render(request, 'main/index.html', context)
@@ -63,14 +65,13 @@ def make_map_with_filter_options(post_data):
                                          user_skill__lte=post_data['user_skill'],
                                          enviroment_chars__exact=post_data['enviroment_chars'],
                                          extreme__lte=post_data['extreme'])
-    # print(activities)
     for el in activities:
         location = [float(el.coords.split(',')[0]), float(el.coords.split(',')[1])]
         folium.Marker(location=location, popup=el.title, icon=folium.Icon(color=colors[el.type])).add_to(base_map)
 
     filter_map = base_map._repr_html_()
 
-    return filter_map
+    return filter_map, activities
 
 
 def add_activities_to_db(list_of_activities):
